3eef57371e7e8ca494cf97700aaa1d244ca1343a
marcocarneiro <marco.carneiro7@gmail.com>
2022-09-12 11:56:41 -0300

    Validação de formulário

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,50 @@
+/*  ///////////////////  FUNÇÕES   ////////////////////  */
+const aviso = (msg)=> {
+    alert(msg)
+}
+
+const mostraIdade = ()=>{
+    let span = document.getElementById('txt-idade')
+    let campoIdade = document.getElementById('idade')
+    span.innerText = campoIdade.value
+}
+
+
+
+
+
+/* ---------------------------------------------------- */
+
+
+
+
+
+/* //////////// EVENTOS E EXECUÇÕES AUTOMÁTICAS  /////////// */
+
+mostraIdade()
+document.getElementById('idade').addEventListener('change', mostraIdade)
 
 //Inicializa animações scroll do AOS
 AOS.init();
+
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+( ()=> {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
