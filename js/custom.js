79c81d8a60a4b4a748bb026f6ed67ed400158d49
marcocarneiro <marco.carneiro7@gmail.com>
2022-09-13 12:23:06 -0300

    Consumo de API

--- js/custom.js
+++ js/custom.js
@@ -9,6 +9,30 @@ const mostraIdade = ()=>{
     span.innerText = campoIdade.value
 }
 
+var dataAtual = new Date()
+const monstraData = ()=> {
+  let dia = dataAtual.getDay()
+  let mes = dataAtual.getMonth() + 1
+  let ano = dataAtual.getFullYear()
+  let hora = dataAtual.getHours()
+  let valor = dia +'/'+ mes + '/' + ano + ' - ' + hora
+
+  document.getElementById('dt-cadastro').value = valor
+}
+
+//Preenche o select "estado" com os estados da API do IBGE
+const getEstados = ()=>{
+  let api = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
+  let select = document.getElementById('estado')
+
+  //Lê a API através do fetch(), 1o then captura os dados, 2o then trata os dados
+  fetch(api).then(resposta => resposta.json()).then(json => {
+    var options = '<option>Selecione</option>'
+    
+    select.innerHTML = options
+  })
+
+}
 
 
 
@@ -16,29 +40,30 @@ const mostraIdade = ()=>{
 /* ---------------------------------------------------- */
 
 
-
-
-
 /* //////////// EVENTOS E EXECUÇÕES AUTOMÁTICAS  /////////// */
+
+getEstados()
 
 mostraIdade()
 document.getElementById('idade').addEventListener('change', mostraIdade)
 
+monstraData()
+
 //Inicializa animações scroll do AOS
 AOS.init();
 
-
-// Example starter JavaScript for disabling form submissions if there are invalid fields
+// Impede o envio do formulário quando os campos estão inválidos
 ( ()=> {
     'use strict'
   
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    //váriavel captura as tags <form> que contém a classe "needs-validation"
     var forms = document.querySelectorAll('.needs-validation')
   
-    // Loop over them and prevent submission
+    // Executa para cada formulário da variável forms
     Array.prototype.slice.call(forms)
       .forEach(function (form) {
         form.addEventListener('submit', function (event) {
+          //Se houver campos inválidos, interrompe o SUBMIT
           if (!form.checkValidity()) {
             event.preventDefault()
             event.stopPropagation()
@@ -48,3 +73,27 @@ AOS.init();
         }, false)
       })
   })()
+
+
+
+/* 
+const getEstados = ()=>{
+  let api = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
+  let select = document.getElementById('estado')
+
+  //Lê a API através do fetch(), 1o then captura os dados, 2o then trata os dados
+  fetch(api).then(resposta => resposta.json()).then(json => {
+    var options = '<option>Selecione</option>'
+    //Executa em cada item do JSON
+    for (const key in json) {
+      options += '<option>'+json[key].nome+'</option>'
+    }
+
+    select.innerHTML = options
+  })
+
+}
+*/
+
+
+
